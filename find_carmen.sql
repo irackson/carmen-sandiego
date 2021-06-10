8dacdd8c08124cee210abac2b1e219fa3a20b761
irackson <nasirus@protonmail.com>
2021-06-10 01:12:10 -0400

    fix unicode

--- find_carmen.sql
+++ find_carmen.sql
@@ -49,7 +49,7 @@ WHERE countrycode = (
  (1 row)
  
  */
--- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on to a different country, a country where people speak only the language she was learning. Find out which nearby country speaks nothing but that language.
+-- Clue #3: We have new news on the classes Carmen attended - our gumshoes tell us she's moved on to a different country, a country where people speak only the language she was learning. Find out which nearby country speaks nothing but that language.
 SELECT countrycode
 FROM countrylanguage
 WHERE language = (
@@ -72,7 +72,7 @@ WHERE language = (
  (1 row)
  
  */
--- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
+-- Clue #4: We're booking the first flight out - maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country - that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
 SELECT name
 FROM city
 WHERE countrycode = (
@@ -120,7 +120,7 @@ WHERE countrycode = (
  (1 row)
  
  */
--- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
+-- Clue #5: Oh no, she pulled a switch - there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 SELECT countrycode
 FROM city
 WHERE name LIKE CONCAT(
@@ -292,11 +292,11 @@ WHERE id = (
  (1 row)
  
  */
--- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock.
--- Lucky for us, she's getting cocky. She left us a note, and I'm sure she thinks she's very clever, but if we can crack it, we can finally put her where she belongs – behind bars.
--- Our playdate of late has been unusually fun –
+-- Clue #7: She knows we're on to her - her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock.
+-- Lucky for us, she's getting cocky. She left us a note, and I'm sure she thinks she's very clever, but if we can crack it, we can finally put her where she belongs - behind bars.
+-- Our playdate of late has been unusually fun -
 -- As an agent, I'll say, you've been a joy to outrun.
--- And while the food here is great, and the people – so nice!
+-- And while the food here is great, and the people - so nice!
 -- I need a little more sunshine with my slice of life.
 -- So I'm off to add one to the population I find
 -- In a city of ninety-one thousand and now, eighty five.
